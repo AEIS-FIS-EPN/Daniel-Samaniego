851815c5c9b3a5f2e91cf21f7a0379b49d021461
Daniel-Samaniego <daniel.samaniego@epn.edu.ec>
2024-04-04 23:02:40 -0500

    feat: add component tittle and install lint.  Install husky and create precommit hoot to use lint.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    // eslint-disable-next-line no-undef, react/react-in-jsx-scope
+    <div className="App">
+      <div>Daniel Samaniego</div>
+    </div>
+  )
+}
+
+export default App
